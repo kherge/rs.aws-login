e1d29cdf983496a058cb34ba65a51258375e284c
Kevin Herrera <kevin@herrera.io>
2021-12-20 21:15:12 +0000

    Creating the main() function.

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+//! Provides the main function for accessing the application.
+
+/// Initializes the application and processes the command line arguments.
+fn main() {
+    println!("Hello, world!");
+}
