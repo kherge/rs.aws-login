c8f6d5d20f439f924f098388e6c883fb2c9a419c
Kevin Herrera <kevin@herrera.io>
2021-12-23 17:29:51 +0000

    Creating debug logging macro.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,23 @@
+//! Provides utilities that may be shared by multiple subcommands.
+
 mod command;
 mod interface;
 
 pub use command::*;
 pub use interface::*;
+
+/// Prints a debugging message in debug builds.
+#[cfg(debug_assertions)]
+#[macro_export]
+macro_rules! debug {
+    ($message:tt, $($args:tt)*) => {
+        eprintln!($message, $($args)*);
+    };
+}
+
+/// Removes debug logs in release builds.
+#[cfg(not(debug_assertions))]
+#[macro_export]
+macro_rules! debug {
+    ($message:tt, $($args:tt)*) => {};
+}
