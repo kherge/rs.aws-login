cd36b066ada734228c551dfe5f3424e937a8982a
Kevin Herrera <kevin@herrera.io>
2021-12-29 04:59:42 +0000

    Adding a shell clean up step.

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,24 @@ mod util;
 
 /// Initializes the application and processes the command line arguments.
 fn main() {
-    let app = app::Application::from_args();
+    let app = app::Application::from_args_safe();
     let mut stderr = io::stderr();
     let mut stdout = io::stdout();
 
-    if let Err(error) = app.execute(&mut stderr, &mut stdout) {
-        error.exit(&mut stderr);
+    // Run the application.
+    if let Ok(app) = &app {
+        if let Err(error) = app.execute(&mut stderr, &mut stdout) {
+            error.exit(&mut stderr);
+        }
+    }
+
+    // Exit evaluation loop if we're in one.
+    if let Some(shell) = util::SHELL.lock().unwrap().as_mut() {
+        shell.exit();
+    }
+
+    // Finally, exit with clap errors if available.
+    if let Err(error) = &app {
+        error.exit();
     }
 }
