3da28509381d552a3374e93032fc4e7e6a15eb63
Kevin Herrera <kevin@herrera.io>
2021-12-29 04:26:25 +0000

    Simplifying menu prompt.

--- src/util/interface.rs
+++ src/util/interface.rs
@@ -1,20 +1,15 @@
 //! Provides utilities for the command line interface.
 
-use console::Term;
 use dialoguer::{theme::ColorfulTheme, Select};
 
 /// Prompts the user to select a choice from a list.
 pub fn choose<'a, 'b, S: ToString>(prompt: &'a str, items: &'b [S]) -> &'b S {
-    let mut selected: Option<usize> = None;
+    let selected = Select::with_theme(&ColorfulTheme::default())
+        .items(items)
+        .with_prompt(prompt)
+        .default(0)
+        .interact()
+        .unwrap();
 
-    while selected.is_none() {
-        selected = Select::with_theme(&ColorfulTheme::default())
-            .items(items)
-            .with_prompt(prompt)
-            .default(0)
-            .interact_on_opt(&Term::stderr())
-            .unwrap();
-    }
-
-    &items[selected.unwrap()]
+    &items[selected]
 }
