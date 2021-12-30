ae0a7a5766e0ebb8ea6dcc85120cda29b18ce10f
Kevin Herrera <kevin@herrera.io>
2021-12-30 18:12:00 +0000

    Documenting how the main function is intended to work.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ mod util;
 
 use structopt::StructOpt;
 
+/// Primary entrypoint to the command line interface.
+///
+/// A new [`app::Application`] instance is created from any command line arguments that may have
+/// been provided by the user. If an instance was successfully constructed, an attempt is made to
+/// execute the requested subcommand. If the subcommand returns an error response, a clean up
+/// operation is performed before the process exits with the error that was returned. If the
+/// `app::Application` instance could not be created, a clean up operation is still performed
+/// before exiting with the `clap::Error` returned by [`structopt::StructOpt::from_args_safe`].
 fn main() {
     let mut app = app::Application::from_args_safe();
 
