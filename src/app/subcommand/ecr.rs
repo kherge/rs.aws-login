f64a24407019a0b010ff14da6ea6cc094cd484da
Kevin Herrera <kevin@herrera.io>
2021-12-31 23:02:54 +0000

    Recreating the ECR subcommand.

---
+++ src/app/subcommand/ecr.rs
@@ -0,0 +1,70 @@
+//! A subcommand used to configure Docker to use the AWS Elastic Container Registry.
+
+use crate::app::ErrorContext;
+use crate::util::run;
+use crate::{app, err};
+
+/// The options for the subcommand.
+#[derive(structopt::StructOpt)]
+pub struct Subcommand {}
+
+impl app::Execute for Subcommand {
+    fn execute(&self, context: &mut impl app::Context) -> app::Result<()> {
+        let registry_uri = generate_registry_uri(context)?;
+        let password = run::Run::new("aws")
+            .with_aws_options(context)
+            .arg("ecr")
+            .arg("get-login-password")
+            .output()
+            .with_context("Could not generate ECR password.".to_owned())?;
+
+        run::Run::new("docker")
+            .arg("login")
+            .arg("--username")
+            .arg("AWS")
+            .arg("--password")
+            .arg(&password)
+            .arg(&registry_uri)
+            .pass_through(context)
+            .with_context("Docker could not be configured to use the registry.".to_owned())?;
+
+        Ok(())
+    }
+}
+
+/// Generates the ECR registry URI using the active profile.
+fn generate_registry_uri(context: &impl app::Context) -> app::Result<String> {
+    let account_id = run::Run::new("aws")
+        .with_aws_options(context)
+        .arg("sts")
+        .arg("get-caller-identity")
+        .arg("--query")
+        .arg("Account")
+        .arg("--output")
+        .arg("text")
+        .output()
+        .map(|output| output.trim().to_owned())
+        .with_context("Could not get account ID from AWS CLI.".to_owned())?;
+
+    let region = match context.region() {
+        Some(region) => region.to_owned(),
+        None => {
+            let output = run::Run::new("aws")
+                .with_aws_options(context)
+                .arg("configure")
+                .arg("get")
+                .arg("region")
+                .output()
+                .map(|output| output.trim().to_owned())
+                .with_context("Could not get default region from AWS CLI.".to_owned())?;
+
+            if output.is_empty() {
+                err!(1, "The region could not be determined.");
+            }
+
+            output
+        }
+    };
+
+    Ok(format!("{}.dkr.ecr.{}.amazonaws.com", account_id, region))
+}
--- src/app/subcommand/mod.rs
+++ src/app/subcommand/mod.rs
@@ -1,6 +1,7 @@
 //! Provides the application subcommands to be executed.
 
 mod debug;
+mod ecr;
 
 use crate::app;
 
@@ -16,11 +17,20 @@ pub enum Subcommand {
     /// Prints debugging messages for the application.
     #[cfg(debug_assertions)]
     Debug(debug::Subcommand),
+
+    /// Configures Docker to use AWS ECR.
+    ///
+    /// This subcommand will generate the registry URI for the account in your active AWS CLI
+    /// profile, generate a new ECR password, and use the Docker client to log into the AWS ECR
+    /// service.
+    Ecr(ecr::Subcommand),
 }
 
 impl app::Execute for Subcommand {
     fn execute(&self, context: &mut impl app::Context) -> app::Result<()> {
         match self {
+            Self::Ecr(cmd) => cmd.execute(context),
+
             #[cfg(debug_assertions)]
             Self::Debug(cmd) => cmd.execute(context),
         }
