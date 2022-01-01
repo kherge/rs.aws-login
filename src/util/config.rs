fa314ce4f74cedbabf84eb7d9dafe1c508049873
Kevin Herrera <kevin@herrera.io>
2022-01-01 19:54:12 +0000

    Finding the name of the application.

--- src/util/config.rs
+++ src/util/config.rs
@@ -1,6 +1,6 @@
 //! Manages the configuration settings and files for the application.
 
-use std::path;
+use std::{env, path};
 
 lazy_static::lazy_static! {
     /// The path to the AWS CLI configuration directory.
@@ -12,6 +12,18 @@ lazy_static::lazy_static! {
         },
         None => panic!("The home directory could not be determined."),
     };
+
+    /// The name of the binary being executed, which should be this application.
+    ///
+    /// The processes that depend on this value will all assume that the binary is available in
+    /// the user's `PATH`. The installation guide must include information on this requirement to
+    /// prevent any confusion on their end.
+    pub static ref BIN_NAME: String = env::args()
+        .nth(0)
+        .map(|name| path::PathBuf::from(name))
+        .and_then(|path| path.file_name())
+        .map(|result| result.to_string_lossy().to_string())
+        .unwrap_or_else(|| "Unable to determine the name of this application.".to_owned());
 
     /// The path to the application configuration directory.
     pub static ref CONFIG_DIR: path::PathBuf = match home::home_dir() {
