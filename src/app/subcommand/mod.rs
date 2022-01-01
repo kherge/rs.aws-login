eabb6caeec56bbf088ce2801f95771c8bbc4a595
Kevin Herrera <kevin@herrera.io>
2022-01-01 17:28:35 +0000

    Partially recreating the profile subcommand.
    The only missing part is the export of AWS_PROFILE into the current shell environment. The next few commits should introduce shell modification capabilities to the application.

--- src/app/subcommand/mod.rs
+++ src/app/subcommand/mod.rs
@@ -3,6 +3,7 @@
 mod debug;
 mod ecr;
 mod eks;
+mod profile;
 mod sso;
 
 use crate::app;
@@ -34,6 +35,15 @@ pub enum Subcommand {
     /// will be updated to support accessing it.
     Eks(eks::Subcommand),
 
+    /// Makes an AWS CLI profile the active profile.
+    ///
+    /// This subcommand will first check if the profile exists. If the profile does not exist but
+    /// has a corresponding template, the AWS CLI profile will be created using the template. If
+    /// the AWS CLI profile exists, or was created, it will be made an active AWS CLI profile.
+    /// This activation removes the need to use the --profile option for every AWS CLI execution.
+    #[structopt(name = "use")]
+    Profile(profile::Subcommand),
+
     /// Logs into an AWS account using SSO.
     ///
     /// This subcommand will attempt to log into the AWS account configured for the active AWS CLI
@@ -48,6 +58,7 @@ impl app::Execute for Subcommand {
         match self {
             Self::Ecr(cmd) => cmd.execute(context),
             Self::Eks(cmd) => cmd.execute(context),
+            Self::Profile(cmd) => cmd.execute(context),
             Self::Sso(cmd) => cmd.execute(context),
 
             #[cfg(debug_assertions)]
---
+++ src/app/subcommand/profile.rs
@@ -0,0 +1,71 @@
+//! A subcommand used to create and/or select an AWS CLI profile.
+
+use crate::app::{self, profile, ErrorContext};
+use crate::err;
+use crate::util::{run, term};
+
+/// The options for the subcommand.
+#[derive(structopt::StructOpt)]
+pub struct Subcommand {}
+
+impl app::Execute for Subcommand {
+    fn execute(&self, context: &mut impl app::Context) -> app::Result<()> {
+        let existing = get_existing_profiles(context)?;
+        let profiles = profile::get_profiles()?;
+        let profile = match context.profile() {
+            Some(profile) => profile,
+            None => term::select(
+                "Please select a profile to use:",
+                &profiles
+                    .iter()
+                    .map(|(_, v)| v)
+                    .collect::<Vec<&profile::Profile>>(),
+            )
+            .map(|p| p.name())
+            .with_context(|| "Unable to selecet a profile.".to_owned())?,
+        };
+
+        if !existing.iter().any(|p| p == profile) {
+            if let Some(profile) = profiles.get(profile) {
+                create_profile(context, profile)?;
+            } else {
+                err!(1, "The profile, {}, does not exist.", profile);
+            }
+        }
+
+        // TODO export AWS_PROFILE={}
+
+        Ok(())
+    }
+}
+
+/// Creates the AWS CLI profile.
+fn create_profile(context: &mut impl app::Context, profile: &profile::Profile) -> app::Result<()> {
+    for (key, value) in profile.settings() {
+        run::Run::new("aws")
+            .with_aws_options(context)
+            .arg("configure")
+            .arg("set")
+            .arg(key)
+            .arg(value)
+            .pass_through(context)
+            .with_context(|| format!("Could not set the profile setting, {}.", key))?;
+    }
+
+    Ok(())
+}
+
+/// Returns a list of existing AWS CLI profiles.
+fn get_existing_profiles(context: &impl app::Context) -> app::Result<Vec<String>> {
+    let profiles = run::Run::new("aws")
+        .with_aws_options(context)
+        .arg("configure")
+        .arg("list-profiles")
+        .output()
+        .with_context(|| "Could not get a list of existing AWS CLI profiles.".to_owned())?
+        .split_whitespace()
+        .map(|s| s.to_owned())
+        .collect();
+
+    Ok(profiles)
+}
