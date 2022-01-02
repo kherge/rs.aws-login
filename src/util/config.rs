3cc30bab25f10381cd7cf831cc25bfcb952961f0
Kevin Herrera <kevin@herrera.io>
2022-01-02 19:03:12 +0000

    Automatically creating config directory if it does not exist.
    Because of the lazy evaluation of... lazy_static, the creation will not occur until the path is used.

--- src/util/config.rs
+++ src/util/config.rs
@@ -1,6 +1,6 @@
 //! Manages the configuration settings and files for the application.
 
-use std::{env, path};
+use std::{env, fs, path};
 
 lazy_static::lazy_static! {
     /// The path to the AWS CLI configuration directory.
@@ -19,19 +19,28 @@ lazy_static::lazy_static! {
         .expect("Could not create a string for the application name.");
 
     /// The path to the application configuration directory.
-    pub static ref CONFIG_DIR: path::PathBuf = match home::home_dir() {
-        Some(mut path) => {
-            if cfg!(windows) {
-                path.push("AppData");
-                path.push("Roaming");
-                path.push("AWS Login");
-            } else {
-                path.push(".config");
-                path.push("aws-login");
-            }
+    pub static ref CONFIG_DIR: path::PathBuf = {
+        let path = match home::home_dir() {
+            Some(mut path) => {
+                if cfg!(windows) {
+                    path.push("AppData");
+                    path.push("Roaming");
+                    path.push("AWS Login");
+                } else {
+                    path.push(".config");
+                    path.push("aws-login");
+                }
 
-            path
-        },
-        None => panic!("The home directory could not be determined."),
+                path
+            },
+            None => panic!("The home directory could not be determined."),
+        };
+
+        if !path.exists() {
+            fs::create_dir_all(&path)
+                .unwrap_or_else(|_| panic!("The configuration directory could not be created."));
+        }
+
+        path
     };
 }
