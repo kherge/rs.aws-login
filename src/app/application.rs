e62b1f9dcd1ea05cda058302ec9e7a8b96eb853a
Kevin Herrera <kevin@herrera.io>
2022-01-17 03:53:06 +0000

    Modifying Application to implement Main and Shared traits.

--- src/app/application.rs
+++ src/app/application.rs
@@ -1,12 +1,26 @@
 //! Provides the primary command line application interface.
 
-use crate::app::{subcommand, Execute};
-use std::{io, sync};
+use crate::app::subcommand::Subcommand;
+use carli::io::Stream;
+use carli::prelude::app::*;
+use std::{cell, io};
 
 /// Manages the global command line options.
 #[derive(clap::Parser)]
 #[clap(about, version, author)]
 pub struct Application {
+    /// The error output stream.
+    #[clap(skip = cell::RefCell::new(io::stderr().into()))]
+    error: cell::RefCell<Stream>,
+
+    /// The input stream.
+    #[clap(skip = cell::RefCell::new(io::stdin().into()))]
+    input: cell::RefCell<Stream>,
+
+    /// The error output stream.
+    #[clap(skip = cell::RefCell::new(io::stdout().into()))]
+    output: cell::RefCell<Stream>,
+
     /// Overrides the active AWS CLI profile.
     #[clap(long, global = true)]
     profile: Option<String>,
@@ -17,76 +31,25 @@ pub struct Application {
 
     /// The subcommand to execute.
     #[clap(subcommand)]
-    subcommand: subcommand::Subcommand,
+    subcommand: Subcommand,
 }
 
-impl Application {
-    /// Creates a new context and executes the requested subcommand.
-    ///
-    /// ```
-    /// use structopt::StructOpts;
-    ///
-    /// Application::from_args().execute();
-    /// ```
-    pub fn execute(&self) -> super::Result<()> {
-        let mut context = ApplicationContext::new(self);
-
-        self.subcommand.execute(&mut context)
+impl Main for Application {
+    fn subcommand(&self) -> &dyn carli::command::Execute<Self> {
+        &self.subcommand
     }
 }
 
-/// Manages the context in which subcommands are executed.
-struct ApplicationContext {
-    /// The error output stream.
-    error: sync::Arc<sync::Mutex<io::Stderr>>,
-
-    /// The standard output stream.
-    output: sync::Arc<sync::Mutex<io::Stdout>>,
-
-    /// The name of the AWS CLI profile.
-    profile: Option<String>,
-
-    /// The name of the AWS region.
-    region: Option<String>,
-}
-
-impl ApplicationContext {
-    /// Creates a new instance using the global options from the application.
-    ///
-    /// The `profile` and `region` options are cloned from the original application instance, but
-    /// new instances of [`io::Stderr`] and [`io::Stdout`] are created for their respective error
-    /// and standard output stream.
-    ///
-    /// ```
-    /// use structopt::StructOpts;
-    ///
-    /// let application = Application::from_args();
-    /// let mut context = ApplicationContext::new(&application);
-    /// ```
-    fn new(application: &Application) -> Self {
-        Self {
-            error: sync::Arc::new(sync::Mutex::new(io::stderr())),
-            output: sync::Arc::new(sync::Mutex::new(io::stdout())),
-            profile: application.profile.clone(),
-            region: application.region.clone(),
-        }
-    }
-}
-
-impl super::Context for ApplicationContext {
-    fn error(&mut self) -> sync::Arc<sync::Mutex<dyn io::Write>> {
-        self.error.clone()
+impl Shared for Application {
+    fn error(&self) -> cell::RefMut<Stream> {
+        self.error.borrow_mut()
     }
 
-    fn output(&mut self) -> sync::Arc<sync::Mutex<dyn io::Write>> {
-        self.output.clone()
+    fn input(&self) -> cell::RefMut<Stream> {
+        self.input.borrow_mut()
     }
 
-    fn profile(&self) -> Option<&str> {
-        self.profile.as_deref()
-    }
-
-    fn region(&self) -> Option<&str> {
-        self.region.as_deref()
+    fn output(&self) -> cell::RefMut<Stream> {
+        self.output.borrow_mut()
     }
 }
